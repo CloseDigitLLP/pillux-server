6ba8710c73bdaa8e4c9867a787baed9f8789a229
aakashdave <aakash@closedigit.com>
2023-06-01 19:45:17 +0530

    debits module backend

--- api/debits/controllers/main.js
+++ api/debits/controllers/main.js
@@ -1,15 +1,15 @@
-module.exports={
-    list: async (req, res) => {
+module.exports = {
+    list:async (req,res)=>{
         try {
-            let alerts = await framework.services.alerts.basic.fetch()
-            if (!alerts.length) {
+            let debits=await framework.services.debits.basic.fetch();
+            if(!debits.length){
                 res.status(404).json({
                     message: 'no records found!',
                     error: true,
                     data: {}
                 })
-            } else {
-                let data = alerts;
+            }else{
+                let data=debits;
                 res.status(200).json({
                     message: '',
                     error: false,
@@ -24,5 +24,5 @@ module.exports={
                 data: error
             })
         }
-    },
+    }
 }

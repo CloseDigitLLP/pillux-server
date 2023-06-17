fbd371855821946236c877a24350704420db6a59
Sagar Ratanpara <sagar@closedigit.com>
2023-06-17 17:24:27 +0530

    vehicle table changes and new vehicle_types table added done

---
+++ db/models/vehicle_types.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class vehicle_types extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      vehicle_types.hasMany(models.vehicle,{foreignKey:"type_id",as:"VehicleTypes",onDelete: "CASCADE", onUpdate: "CASCADE"})
+    }
+  }
+  vehicle_types.init({
+    type: DataTypes.STRING,
+  }, {
+    sequelize,
+    modelName: 'vehicle_types',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  });
+  return vehicle_types;
+};

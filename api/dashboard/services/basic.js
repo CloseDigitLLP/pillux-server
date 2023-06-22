7100b580deec3a6ea834062ce0165e0e7a66a6a3
aakashdave <aakash@closedigit.com>
2023-06-22 17:58:27 +0530

    changes in logic of dashboard

--- api/dashboard/services/basic.js
+++ api/dashboard/services/basic.js
@@ -1,86 +1,79 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize } = require('sequelize');
 
 module.exports = {
   fetch: async () => {
     try {
-      const noOfMonitors= await framework.models.users.count({
+      const noOfMonitors = await framework.models.users.count({
         where: { role_id: 2 },
       });
+      // **********************************************************************
+      // MOTIF - 0 <--- For Code type of Exams
+      // MOTIF - 1 <--- For Conduite types of Exams
 
       const result = await framework.models.planning_exams.findOne({
         attributes: [
-          [Sequelize.literal('COUNT(CASE WHEN status = "pass" THEN id END)'), 'examPassedInWeek'],
-          [Sequelize.literal('COUNT(id)'), 'studentInWeek']
+          [Sequelize.literal('COUNT(CASE WHEN status = "pass" AND motif=0 THEN 1 END)'), 'codeTypesInWeek'],
+          [Sequelize.literal('COUNT(CASE WHEN status = "pass" AND motif=1 THEN 1 END)'), 'conduitTypesInWeek'],
+          [Sequelize.literal('COUNT(CASE WHEN status = "pass" THEN 1 END)'),'passedStudentInWeek']
         ],
         where: {
           created_at: {
-            [Sequelize.Op.gt]: Sequelize.literal("current_date() - interval 7 day")
-          }
-        }
+            [Sequelize.Op.gt]: Sequelize.literal('current_date() - interval 7 day'),
+          },
+        },
       });
-      const examPassedInWeek = result.get('examPassedInWeek');
-      const studentInWeek = result.get('studentInWeek');
-      
+      const codeTypesInWeek = result.get('codeTypesInWeek');
+      const conduitTypesInWeek = result.get('conduitTypesInWeek');
+      const passedStudentInWeek = result.get('passedStudentInWeek');
+
       const currentDate = new Date();
       const oneMonthAgo = new Date();
       oneMonthAgo.setMonth(oneMonthAgo.getMonth() - 1);
       const result1 = await framework.models.planning_exams.findOne({
         attributes: [
-          [Sequelize.fn('COUNT', Sequelize.literal('DISTINCT id')), 'studentInMonth'],
-          [Sequelize.fn('COUNT', Sequelize.literal('CASE WHEN status = "pass" THEN 1 END')), 'examPassedInMonth']
+          [
+            Sequelize.fn('COUNT', Sequelize.literal('CASE WHEN status = "pass" AND motif=0 THEN 1 END')),
+            'codeTypesInMonth',
+          ],
+          [
+            Sequelize.fn('COUNT', Sequelize.literal('CASE WHEN status = "pass" AND motif=1 THEN 1 END')),
+            'conduitTypesInMonth',
+          ],
         ],
         where: {
           created_at: {
             [Sequelize.Op.gt]: oneMonthAgo,
-            [Sequelize.Op.lte]: currentDate
-          }
-        }
+            [Sequelize.Op.lte]: currentDate,
+          },
+        },
       });
+      const codeTypesInMonth = result1.get('codeTypesInMonth');
+      const conduitTypesInMonth = result1.get('conduitTypesInMonth');
       
-      const studentInMonth = result1.get('studentInMonth');
-      const examPassedInMonth = result1.get('examPassedInMonth');
-    //   const studentInWeek=await framework.models.planning_exams.count({
-    //     where:{
-    //         created_at:{
-    //             [Sequelize.Op.gt] : Sequelize.literal("current_date() - interval 7 day")
-    //          }
-    //     }
-    //   })
-    //   const examPassedInWeek=await framework.models.planning_exams.count({
-    //     where: {
-    //          status:"pass",
-    //          created_at:{
-    //             [Sequelize.Op.gt] : Sequelize.literal("current_date() - interval 7 day")
-    //          }
-    //     }
-    //   })
-    //   const studentInMonth=await framework.models.planning_exams.count({
-    //     where:{
-    //         created_at:{
-    //             [Sequelize.Op.gt] : oneMonthAgo,
-    //             [Sequelize.Op.lte]: currentDate
-    //          }
-    //     }
-    //   })
-    //   const examPassedInMonth=await framework.models.planning_exams.count({
-    //     where: {
-    //          status:"pass",
-    //          created_at:{
-    //             [Sequelize.Op.gt] : oneMonthAgo,
-    //             [Sequelize.Op.lte]: currentDate
-    //          }
-    //     }
-    //   })
 
-      const newStudents=await framework.models.students.count({
+      const newStudents = await framework.models.students.count({
         where: {
-            created_at : {
-                [Sequelize.Op.gt] : Sequelize.literal("current_date() - interval 7 day ")
-            }
-        }
-      })
-       
-      return {monitors:noOfMonitors ,studentInWeek, examPassedInWeek,studentInMonth ,examPassedInMonth, newStudents };
+          created_at: {
+            [Sequelize.Op.gt]: Sequelize.literal('current_date() - interval 7 day '),
+          },
+        },
+      });
+
+      // const totalPassedStudWeek=await framework.models.planning_exams.count({
+      //   where:{
+
+      //   }
+      // })
+
+      return {
+        monitors: noOfMonitors,
+        codeTypesInWeek,
+        conduitTypesInWeek,
+        codeTypesInMonth,
+        conduitTypesInMonth,
+        newStudents,
+        passedStudentInWeek
+      };
     } catch (error) {
       console.log('Error => ', error);
       return Promise.reject(error);

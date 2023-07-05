cff2633e1cd759f682db16eb8302e844dd4e5677
rajan <rajanvasani9@gmail.com>
2023-07-05 23:44:10 +0530

    extra student info added in list api for remaining hours validation

--- api/students/controllers/main.js
+++ api/students/controllers/main.js
@@ -19,7 +19,8 @@ module.exports = {
                         address: student.address,
                         neph: student.neph,
                         date_code: student.date_code,
-                        drivingschool_id : student.drivingschool_id
+                        drivingschool_id : student.drivingschool_id,
+                        studentFormula: student.studentFormula
                     }
                 })
                 res.status(200).json({

4bf985c0d821b600f55985199fde1673c02c81e3
jenish <jenish@closedigit.com>
2023-07-20 15:16:44 +0530

    mobile api changes

--- api/auth/controllers/auth.js
+++ api/auth/controllers/auth.js
@@ -40,4 +40,44 @@ module.exports = {
       });
     }
   },
+  monitorLogin: async (req, res) => {
+    try {
+      var { email, password } = req.body;
+      password = md5(password);
+
+      let user = await framework.services.auth.authentication.login(
+        email,
+        password
+      );
+      if (!user || user?.usersRole?.name !== 'Moniteurs' || !user?.usersRole) {
+        return res.status(404).send({
+          message: "User not found",
+          error: true,
+          data: {},
+        });
+      } else {
+        user = JSON.parse(JSON.stringify(user));
+        let { accessToken } = framework.functions.jwt.getMonitorAccessToken(user);
+        let { refreshToken } = framework.functions.jwt.getRefreshToken(user);
+        let exp = framework.functions.jwt.expiresIn(accessToken);
+        user = {
+          ...user,
+          accessToken,
+          refreshToken,
+          exp,
+        };
+        return res.send({
+          message: "",
+          error: false,
+          data: user,
+        });
+      }
+    } catch (e) {
+      return res.status(400).send({
+        message: e.message,
+        error: true,
+        data: e.message,
+      });
+    }
+  },
 };

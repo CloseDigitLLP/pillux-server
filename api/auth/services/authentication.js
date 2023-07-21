1650e5c10e58eef9a262425c3a8f5374306116e0
Sagar Ratanpara <sagar@closedigit.com>
2023-07-21 18:44:31 +0530

    chnages on relation for fetching vehicle list in instructor login

--- api/auth/services/authentication.js
+++ api/auth/services/authentication.js
@@ -1,52 +1,58 @@
 module.exports = {
-    login: async (email, password) => {
-        try {
-            return await framework.models.users.findOne({
-                where: {
-                    email,
-                    password
-                },
-                attributes: ['email', 'firstname', 'lastname', 'id'],
+  login: async (email, password) => {
+    try {
+      return await framework.models.users.findOne({
+        where: {
+          email,
+          password,
+        },
+        attributes: ['email', 'firstname', 'lastname', 'id'],
+        include: [
+          { model: framework.models.roles, as: 'usersRole', attributes: ['name'] },
+          { model: framework.models.user_drivingschool, as: 'userDrivingschool' },
+        ],
+      });
+    } catch (error) {
+      console.log(error);
+      return Promise.reject(error);
+    }
+  },
+  instructorLogin: async (email, password) => {
+    try {
+      return await framework.models.users.findOne({
+        where: {
+          email,
+          password,
+        },
+        attributes: ['email', 'firstname', 'lastname', 'id'],
+        include: [
+          {
+            model: framework.models.roles,
+            as: 'usersRole',
+            attributes: ['name'],
+          },
+          {
+            model: framework.models.user_drivingschool,
+            as: 'userDrivingschool',
+            include: [
+              {
+                model: framework.models.driving_schools,
+                as: 'drivingSchoolUser',
                 include: [
-                    { model: framework.models.roles, as: "usersRole", attributes: ['name'] },
-                    { model: framework.models.user_drivingschool, as: "userDrivingschool" }
-                ]
-            })
-        } catch (error) {
-            console.log(error)
-            return Promise.reject(error)
-        }
-    },
-    instructorLogin: async (email, password) => {
-        try {
-            return await framework.models.users.findOne({
-                where: {
-                    email,
-                    password
-                },
-                attributes: ['email', 'firstname', 'lastname', 'id'],
-                include: [
-                    { 
-                        model: framework.models.roles, 
-                        as: "usersRole", 
-                        attributes: ['name'] 
-                    },
                     {
-                         model: framework.models.user_drivingschool, 
-                        as: "userDrivingschool",
-                        include: [
-                            {
-                                model: framework.models.vehicles,
-                                as: 'drivingSchoolVehicles',
-                                attributes: ['id','name','immatriculation','date']
-                            }
-                        ] 
+                        model: framework.models.vehicles,
+                        as: 'drivingSchoolVehicles',
+                        attributes: ['id', 'name', 'immatriculation', 'date']
                     }
                 ]
-            })
-        } catch (error) {
-            console.log(error)
-            return Promise.reject(error)
-        }
+              },
+            ],
+          },
+        ],
+      });
+    } catch (error) {
+      console.log(error);
+      return Promise.reject(error);
     }
-}
+  },
+};

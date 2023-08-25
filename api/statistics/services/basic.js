14ee424c9543f435c6498d4cfc6310e7ca236212
Sagar Ratanpara <sagar@closedigit.com>
2023-08-25 17:58:42 +0530

    mobile statistics api added

--- api/statistics/services/basic.js
+++ api/statistics/services/basic.js
@@ -580,4 +580,392 @@ module.exports = {
       return Promise.reject(error);
     }
   },
+
+  totalInstructorHours: async (user, where = {}) => {
+    try {
+      if (user?.usersRole?.name === 'Moniteurs') {
+        where['instructor_id'] = user?.id;
+      }
+
+      const currentYear = new Date().getFullYear();
+      const allMonths = Array.from({ length: 12 }, (_, i) => i + 1);
+      const startYear = 2013;
+      const allYears = Array.from({ length: currentYear - startYear + 1 }, (_, i) => startYear + i);
+
+      const populatedYearlyResults = allYears.map((year) => {
+        return {
+          year,
+          total_hours: 0,
+        };
+      });
+
+      const monthlyResults = await framework.models.planning_generals.findAll({
+        attributes: [
+          [Sequelize.fn('MONTH', Sequelize.col('start_horary')), 'month'],
+          [Sequelize.fn('YEAR', Sequelize.col('start_horary')), 'year'],
+          [
+            Sequelize.literal('CAST(SUM(TIMESTAMPDIFF(SECOND, start_horary, end_horary) / 3600) AS SIGNED)'),
+            'total_hours',
+          ],
+        ],
+        include: [
+          {
+            model: framework.models.users,
+            as: 'instructorGenerals',
+            attributes: [],
+            include: [
+              {
+                model: framework.models.user_drivingschool,
+                as: 'userDrivingschool',
+                attributes: [],
+              },
+            ],
+          },
+        ],
+        where: {
+          ...where,
+          [Op.and]: [
+            Sequelize.where(Sequelize.fn('YEAR', Sequelize.col('start_horary')), currentYear),
+            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('start_horary')), { [Op.in]: allMonths }),
+            {
+              status: {
+                [Op.in]: ['present', 'absent'],
+              },
+            },
+            {
+              motif: {
+                [Op.not]: 'Instructeur absent',
+              },
+            },
+          ],
+        },
+        group: ['month', 'year'],
+        raw: true,
+      });
+
+      const yearlyResults = await framework.models.planning_generals.findAll({
+        attributes: [
+          'instructor_id',
+          [Sequelize.fn('YEAR', Sequelize.col('start_horary')), 'year'],
+          [
+            Sequelize.literal('CAST(SUM(TIMESTAMPDIFF(SECOND, start_horary, end_horary) / 3600) AS SIGNED)'),
+            'total_hours',
+          ],
+        ],
+        include: [
+          {
+            model: framework.models.users,
+            as: 'instructorGenerals',
+            attributes: [],
+            include: [
+              {
+                model: framework.models.user_drivingschool,
+                as: 'userDrivingschool',
+                attributes: [],
+              },
+            ],
+          },
+        ],
+        where: {
+          ...where,
+          [Op.and]: [
+            Sequelize.where(Sequelize.fn('YEAR', Sequelize.col('start_horary')), { [Op.gte]: startYear }),
+            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('start_horary')), { [Op.lte]: currentYear }),
+            {
+              status: {
+                [Op.in]: ['present', 'absent'],
+              },
+            },
+            {
+              motif: {
+                [Op.not]: 'Instructeur absent',
+              },
+            },
+          ],
+        },
+        group: ['instructor_id', 'year'],
+        raw: true,
+      });
+
+      yearlyResults.forEach((yearInfo) => {
+        const yearIndex = yearInfo.year - startYear;
+        if (populatedYearlyResults[yearIndex]) {
+          populatedYearlyResults[yearIndex].total_hours = yearInfo.total_hours;
+        }
+      });
+
+      return {
+        monthly: monthlyResults,
+        yearly: populatedYearlyResults,
+      };
+    } catch (error) {
+      console.log('Error is ==>', error);
+      return Promise.reject(error);
+    }
+  },
+
+  absentInstructorList: async (user, where = {}) => {
+    try {
+      if (user?.usersRole?.name === 'Moniteurs') {
+        where['instructor_id'] = user?.id;
+      }
+
+      return await framewirk.models.planning_generals.findAll({
+        attributes: [
+          [Sequelize.fn('DATE', Sequelize.col('start_horary')), 'date'],
+          [
+            Sequelize.literal('CAST(SUM(TIMESTAMPDIFF(SECOND, start_horary, end_horary) / 3600) AS SIGNED)'),
+            'total_hours',
+          ],
+        ],
+        where: {
+          ...where,
+          [Op.and]: [
+            {
+              status: {
+                [Op.eq]: ['present', 'absent'],
+              },
+            },
+            {
+              motif: {
+                [Op.eq]: 'Instructeur absent',
+              },
+            },
+          ],
+        },
+        group: ['date'],
+        order: [[Sequelize.col('date'), 'DESC']],
+      });
+    } catch (error) {
+      console.log('Error is ==>', error);
+      return Promise.reject(error);
+    }
+  },
+
+  wishlistInstructorCount: async (user, where = {}) => {
+    try {
+      if (user?.usersRole?.name === 'Moniteurs') {
+        where['instructor_id'] = user?.id;
+      }
+
+      return await framewirk.models.exam_wishlist.findAll({
+        attributes: ['instructor_id', [Sequelize.fn('COUNT', Sequelize.col('student_id')), 'studentCount']],
+        where,
+        group: ['instructor_id'],
+      });
+    } catch (error) {
+      console.log('Error is ==>', error);
+      return Promise.reject(error);
+    }
+  },
+
+  averageInstructorHours: async (user, where = {}) => {
+    try {
+      if (user?.usersRole?.name === 'Moniteurs') {
+        where['instructor_id'] = user?.id;
+      }
+
+      const currentYear = new Date().getFullYear();
+      const allMonths = Array.from({ length: 12 }, (_, i) => i + 1);
+      const startYear = 2013;
+      const allYears = Array.from({ length: currentYear - startYear + 1 }, (_, i) => startYear + i);
+
+      const populatedYearlyResults = allYears.map((year) => {
+        return {
+          year,
+          total_hours: 0,
+          total_students: 0,
+          average_hours_per_student: 0,
+        };
+      });
+
+      const populatedMonthlyResults = allMonths.map((month) => {
+        return {
+          month,
+          total_hours: 0,
+          total_students: 0,
+          average_hours_per_student: 0,
+        };
+      });
+
+      const monthlyResults = await framework.models.planning_generals.findAll({
+        attributes: [
+          [Sequelize.fn('MONTH', Sequelize.col('start_horary')), 'month'],
+          [Sequelize.fn('YEAR', Sequelize.col('start_horary')), 'year'],
+          [
+            Sequelize.literal('CAST(SUM(TIMESTAMPDIFF(SECOND, start_horary, end_horary) / 3600) AS SIGNED)'),
+            'total_hours',
+          ],
+          [Sequelize.fn('COUNT', Sequelize.col('student_id')), 'total_students'],
+        ],
+        include: [
+          {
+            model: framework.models.users,
+            as: 'instructorGenerals',
+            attributes: [],
+            include: [
+              {
+                model: framework.models.user_drivingschool,
+                as: 'userDrivingschool',
+                attributes: [],
+              },
+            ],
+          },
+        ],
+        where: {
+          ...where,
+          [Op.and]: [
+            Sequelize.where(Sequelize.fn('YEAR', Sequelize.col('start_horary')), currentYear),
+            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('start_horary')), { [Op.in]: allMonths }),
+            {
+              status: {
+                [Op.in]: ['present', 'absent'],
+              },
+            },
+            {
+              motif: {
+                [Op.not]: 'Instructeur absent',
+              },
+            },
+          ],
+        },
+        group: ['month', 'year'],
+        raw: true,
+      });
+
+      const yearlyResults = await framework.models.planning_generals.findAll({
+        attributes: [
+          'instructor_id',
+          [Sequelize.fn('YEAR', Sequelize.col('start_horary')), 'year'],
+          [
+            Sequelize.literal('CAST(SUM(TIMESTAMPDIFF(SECOND, start_horary, end_horary) / 3600) AS SIGNED)'),
+            'total_hours',
+          ],
+          [Sequelize.fn('COUNT', Sequelize.col('student_id')), 'total_students'],
+        ],
+        include: [
+          {
+            model: framework.models.users,
+            as: 'instructorGenerals',
+            attributes: [],
+            include: [
+              {
+                model: framework.models.user_drivingschool,
+                as: 'userDrivingschool',
+                attributes: [],
+              },
+            ],
+          },
+        ],
+        where: {
+          ...where,
+          [Op.and]: [
+            Sequelize.where(Sequelize.fn('YEAR', Sequelize.col('start_horary')), { [Op.gte]: startYear }),
+            Sequelize.where(Sequelize.fn('MONTH', Sequelize.col('start_horary')), { [Op.lte]: currentYear }),
+            {
+              status: {
+                [Op.in]: ['present', 'absent'],
+              },
+            },
+            {
+              motif: {
+                [Op.not]: 'Instructeur absent',
+              },
+            },
+          ],
+        },
+        group: ['instructor_id', 'year'],
+        raw: true,
+      });
+
+      yearlyResults.forEach((yearInfo) => {
+        const yearIndex = yearInfo.year - startYear;
+        if (populatedYearlyResults[yearIndex]) {
+          populatedYearlyResults[yearIndex].total_hours = yearInfo.total_hours;
+          populatedYearlyResults[yearIndex].total_students = yearInfo.total_students;
+          if (yearInfo.total_students > 0) {
+            populatedYearlyResults[yearIndex].average_hours_per_student =
+              yearInfo.total_hours / yearInfo.total_students;
+          }
+        }
+      });
+
+      monthlyResults.forEach((monthInfo) => {
+        const monthIndex = monthInfo.month - 1;
+        if (populatedMonthlyResults[monthIndex]) {
+          populatedMonthlyResults[monthIndex].total_hours = monthInfo.total_hours;
+          populatedMonthlyResults[monthIndex].total_students = monthInfo.total_students;
+          if (monthInfo.total_students > 0) {
+            populatedMonthlyResults[monthIndex].average_hours_per_student =
+              monthInfo.total_hours / monthInfo.total_students;
+          }
+        }
+      });
+
+      return {
+        monthly: populatedMonthlyResults,
+        yearly: populatedYearlyResults,
+      };
+    } catch (error) {
+      console.log('Error is ==>', error);
+      return Promise.reject(error);
+    }
+  },
+
+  vehicleInstructorTotal: async (user, where = {}) => {
+    try {
+      if (user?.usersRole?.name === 'Moniteurs') {
+        where['instructor_id'] = user?.id;
+      }
+
+      const vehicles = await framework.models.vehicles.findAll({
+        attributes: [
+          'id',
+          'name',
+          [Sequelize.fn('SUM', Sequelize.col('vehicleRepairs.amount')), 'total_repair'],
+          [Sequelize.fn('SUM', Sequelize.col('vehiclePenalty.amount')), 'total_penalty'],
+          [Sequelize.fn('SUM', Sequelize.col('vehicleReport.amount')), 'total_report'],
+        ],
+        include: [
+          {
+            model: framework.models.repairs,
+            as: 'vehicleRepairs',
+            attributes: [],
+          },
+          {
+            model: framework.models.penalties,
+            as: 'vehiclePenalty',
+            attributes: [],
+          },
+          {
+            model: framework.models.report,
+            as: 'vehicleReport',
+            attributes: [],
+          },
+        ],
+        where,
+        group: ['id'],
+      });
+
+      const results = vehicles.map((vehicle) => {
+        const total =
+          (vehicle.dataValues.total_repair || 0) +
+          (vehicle.dataValues.total_penalty || 0) +
+          (vehicle.dataValues.total_report || 0);
+
+        return {
+          id: vehicle.dataValues.id,
+          name: vehicle.dataValues.name,
+          repair: ((vehicle.dataValues.total_repair || 0) / total) * 100 || 0,
+          penalty: ((vehicle.dataValues.total_penalty || 0) / total) * 100 || 0,
+          report: ((vehicle.dataValues.total_report || 0) / total) * 100 || 0,
+        };
+      });
+
+      return results;
+    } catch (error) {
+      console.log('Error is ==>', error);
+      return Promise.reject(error);
+    }
+  },
 };

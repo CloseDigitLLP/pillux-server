57297001b7bdcd06cd8798e00f5974736128e2d4
Sagar Ratanpara <sagar@closedigit.com>
2023-11-02 20:08:56 +0530

    receipt api filter added done

--- api/receipts/controllers/main.js
+++ api/receipts/controllers/main.js
@@ -1,42 +1,78 @@
-const moment = require("moment/moment");
+const moment = require('moment/moment');
 
 module.exports = {
-    list: async (req, res) => {
-        try {
-            let receipts = await framework.services.receipts.basic.fetch(req?.user);
-            if (!receipts.length) {
-                res.status(200).json({
-                    message: 'no records found!',
-                    error: false,
-                    data: []
-                })
-            } else {
-                const Data = receipts?.flatMap((receipt) => {
-                    return receipt?.studentFormula?.flatMap((formula) => {
-                        return formula?.studentFormulaPayment?.map((payment) => ({
-                            date: moment(payment?.created_at)?.format('YYYY/MM/DD'),
-                            fullname: `${receipt?.firstname} ${receipt?.lastname}`,
-                            rules: formula?.formulaId?.name,
-                            amount: payment?.amount,
-                            mode: payment?.mode,
-                            checknumber: payment?.numberbankcheck,
-                            id: payment?.id
-                        }));
-                    });
-                });
-                res.status(200).json({
-                    message: '',
-                    error: false,
-                    data: Data
-                });
-            }
-        } catch (error) {
-            console.log("error =>", error);
-            res.status(500).json({
-                messagae: error?.message,
-                error: true,
-                data: error
-            })
-        }
+  list: async (req, res) => {
+    try {
+      const { startDate, endDate } = req?.query;
+      let receipts = await framework.services.receipts.basic.fetch(req?.user, startDate, endDate);
+      if (!receipts.length) {
+        res.status(200).json({
+          message: 'no records found!',
+          error: false,
+          data: [],
+        });
+      } else {
+        const Data = receipts?.flatMap((receipt) => {
+          return receipt?.studentFormula?.flatMap((formula) => {
+            return formula?.studentFormulaPayment?.map((payment) => ({
+              date: moment(payment?.created_at)?.format('YYYY/MM/DD'),
+              fullname: `${receipt?.firstname} ${receipt?.lastname}`,
+              rules: formula?.formulaId?.name,
+              amount: payment?.amount,
+              mode: payment?.mode,
+              checknumber: payment?.numberbankcheck,
+              id: payment?.id,
+              type: formula?.formulaId?.type,
+            }));
+          });
+        });
+        res.status(200).json({
+          message: '',
+          error: false,
+          data: Data,
+        });
+      }
+    // } else {
+    //     const Data = receipts?.flatMap((receipt) => {
+    //       return receipt?.studentFormula?.flatMap((formula) => {
+    //         return formula?.studentFormulaPayment?.map((payment) => {
+    //           const paymentDate = moment(payment?.created_at);
+    //           if (
+    //             (!day || paymentDate.date() == day) &&
+    //             (!month || paymentDate.month() + 1 == month) &&
+    //             (!year || paymentDate.year() == year)
+    //           ) {
+    //             return {
+    //               date: paymentDate.format('YYYY/MM/DD'),
+    //               fullname: `${receipt?.firstname} ${receipt?.lastname}`,
+    //               rules: formula?.formulaId?.name,
+    //               amount: payment?.amount,
+    //               mode: payment?.mode,
+    //               checknumber: payment?.numberbankcheck,
+    //               id: payment?.id,
+    //               type: formula?.formulaId?.type,
+    //             };
+    //           }
+    //         });
+    //       });
+    //     });
+  
+    //     // Filter out null or undefined entries
+    //     const filteredData = Data.filter((item) => item);
+  
+    //     res.status(200).json({
+    //       message: '',
+    //       error: false,
+    //       data: filteredData,
+    //     });
+    //   }
+    } catch (error) {
+      console.log('error =>', error);
+      res.status(500).json({
+        messagae: error?.message,
+        error: true,
+        data: error,
+      });
     }
-}
+  },
+};
--- api/receipts/services/basic.js
+++ api/receipts/services/basic.js
@@ -1,13 +1,17 @@
 const { Op } = require('sequelize');
+const moment = require('moment');
 
 module.exports = {
-  fetch: async (user, where = {}) => {
+  fetch: async (user, startDate, endDate, where = {}) => {
     try {
       if (user?.usersRole?.name === 'Secrétaires' || user?.usersRole?.name === 'Gérants') {
         where['drivingschool_id'] = {
           [Op.in]: user?.userDrivingschool?.map((ds) => ds.drivingschool_id),
         };
       }
+      let start = moment(startDate, 'DD/MM/YYYY').format("YYYY-MM-DD")
+      let end = moment(endDate, 'DD/MM/YYYY').format("YYYY-MM-DD")
+      
       return await framework.models.students.findAll({
         include: [
           {
@@ -15,21 +19,25 @@ module.exports = {
             as: 'studentFormula',
             attributes: ['formula_id', 'quantity', 'secretary_id'],
             separate: true,
-            order: [['id', 'ASC']],
+            order: [['id', 'DESC']],
             include: [
               {
                 model: framework.models.student_payment,
                 as: 'studentFormulaPayment',
                 attributes: ['mode', 'id', 'amount', 'numberbankcheck', 'created_at', 'updated_at', 'secretary_id'],
-                separate: true,
-                order: [['id', 'ASC']],
+                order: [['id', 'DESC']]
               },
               {
                 model: framework.models.formula,
                 as: 'formulaId',
-                attributes: ['name', 'id', 'price', 'hour'],
+                attributes: ['name', 'id', 'price', 'hour', 'type'],
               },
             ],
+            where: {
+              ['$studentFormulaPayment.created_at$']: {
+                [Op.between]: [start, end],
+              },
+            },
           },
         ],
         attributes: ['id', 'firstname', 'lastname'],
